4127e0a059e222a9f1a9c10a42a6724fc5c0c7a6
Dheerendra <118679439+Dheerendra69@users.noreply.github.com>
2025-06-30 19:33:17 +0530

    Add files via upload

--- api/index.js
+++ api/index.js
@@ -1,8 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
-const mongoose = require("mongoose");
-const exphbs = require("express-handlebars");
+const hbs = require("hbs");
 const userRoutes = require("../routes/userRoutes.js");
 const orderRoutes = require("../routes/orderRoutes");
 const app = express();
@@ -18,21 +17,37 @@ app.use(express.static(publicPath));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Set up handlebars with layout support
-
-// app.engine(
-//   "hbs",
-//   exphbs.engine({
-//     extname: "hbs",
-//     layoutsDir: __dirname + "/public/layouts",
-//     partialsDir: __dirname + "/public/partials",
-//     // No defaultLayout
-//   })
-// );
-
 // setting views engine
 app.set("view engine", "hbs");
 app.set("views", publicPath);
+hbs.registerPartials(path.join(__dirname, "..", "public/partials"));
+
+app.get("/", (req, res) => {
+  res.render("login");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    layout: "layouts/main",
+    title: "About Us",
+  });
+  // res.render("about");
+});
+app.get("/home", (req, res) => {
+  res.render("home", {
+    layout: "layouts/main",
+    title: "Stationary Heaven",
+  });
+  // res.render("home");
+});
 
 app.get("/index", (req, res) => {
   res.render("index");
@@ -49,41 +64,36 @@ app.get("/placeOrder", (req, res) => {
   res.render("placeOrder");
 });
 
-// Route related to form element
-app.post("/checkout", async (req, res) => {
-  res.render("orderPlaced");
-  //   try {
-  //     const { dataField } = req.body;
-  //     const items = JSON.parse(dataField); // Parse the JSON string to extract the items array
-  //     console.log(items);
-
-  //     userOrder = items;
-  //     console.log("Your order is: ");
-  //     console.log(userOrder);
-  //     // sendEmail1();
-
-  //     // Save the order with the extracted items
-  //     const order = new Order({ items });
-  //     await order.save();
-
-  //     // Send a response to the client
-  //     res.redirect("orderPlaced");
-  //   } catch (error) {
-  //     console.log(error);
-  //     res.status(500).json({ error: "Internal Server Error" });
-  //   }
+app.get("/setNewPassword", (req, res) => {
+  res.render("setNewPassword");
 });
 
-app.get("/", (req, res) => {
-  res.render("login");
+app.get("/home", (req, res) => {
+  res.render("home");
 });
 
-app.get("/login", (req, res) => {
-  res.render("login");
+app.get("/labManual", (req, res) => {
+  // res.render("labManual", {
+  //   layout: "layouts/main",
+  //   title: "Lab Manual Page",
+  // });
+  res.render("labManual");
 });
 
-app.get("/signup", (req, res) => {
-  res.render("signup");
+app.get("/productsBiotech", (req, res) => {
+  res.render("productsBiotech");
+});
+
+app.get("/productsMec", (req, res) => {
+  res.render("productsMec");
+});
+
+app.get("/productsElec", (req, res) => {
+  res.render("productsElec");
+});
+
+app.get("/forgotPassword", (req, res) => {
+  res.render("forgotPassword");
 });
 
 app.post("/login", userRoutes);
@@ -115,34 +125,31 @@ app.post("/saveOrder", orderRoutes);
 //   }
 // });
 
-app.get("/setNewPassword", (req, res) => {
-  res.render("setNewPassword");
+// Route related to form element
+app.post("/checkout", async (req, res) => {
+  res.render("orderPlaced");
+  //   try {
+  //     const { dataField } = req.body;
+  //     const items = JSON.parse(dataField); // Parse the JSON string to extract the items array
+  //     console.log(items);
+
+  //     userOrder = items;
+  //     console.log("Your order is: ");
+  //     console.log(userOrder);
+  //     // sendEmail1();
+
+  //     // Save the order with the extracted items
+  //     const order = new Order({ items });
+  //     await order.save();
+
+  //     // Send a response to the client
+  //     res.redirect("orderPlaced");
+  //   } catch (error) {
+  //     console.log(error);
+  //     res.status(500).json({ error: "Internal Server Error" });
+  //   }
 });
 
-// serving static files
-app.get("/home", (req, res) => {
-  res.render("home");
-});
-app.get("/productsBiotech", (req, res) => {
-  res.render("productsBiotech");
-});
-app.get("/productsMec", (req, res) => {
-  res.render("productsMec");
-});
-app.get("/productsElec", (req, res) => {
-  res.render("productsElec");
-});
-app.get("/labManual", (req, res) => {
-  res.render("labManual");
-});
-app.get("/about", (req, res) => {
-  res.render("about", {
-    title: "About Us",
-  });
-});
-app.get("/forgotPassword", (req, res) => {
-  res.render("forgotPassword");
-});
 app.get("*", (req, res) => {
   res.status(404).render("error");
 });

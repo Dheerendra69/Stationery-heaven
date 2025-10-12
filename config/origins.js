e9f81348c603a157236c4b842fe057949123f498
Dheerendra <118679439+Dheerendra69@users.noreply.github.com>
2025-10-12 16:29:15 +0530

    Delete config/origins.js

--- config/origins.js
+++
@@ -1,8 +0,0 @@
-const origins = [
-  "http://localhost:3000",
-  "https://et-manager.netlify.app",
-  "https://event-bookin.netlify.app",
-  "https://journey-compass.netlify.app"
-];
-
-module.exports = origins;

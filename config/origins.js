a1bc9c6dd382635f500a70bab278253179822642
Dheerendra <118679439+Dheerendra69@users.noreply.github.com>
2025-10-12 16:27:17 +0530

    Create origins.js

---
+++ config/origins.js
@@ -0,0 +1,8 @@
+const origins = [
+  "http://localhost:3000",
+  "https://et-manager.netlify.app",
+  "https://event-bookin.netlify.app",
+  "https://journey-compass.netlify.app"
+];
+
+module.exports = origins;
